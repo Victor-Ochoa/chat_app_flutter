f065d83c1952b76986b86fe58f4ecba493617daf
Victor Ochoa <Victor.Ochoa@POSSIBLE.com>
2019-07-01 11:09:27 -0300

    projeto e criação dos itens de ação

---
+++ lib/ui/chat_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: true,
+      top: true,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Chat"),
+          centerTitle: true,
+          elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0 : 4,
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(color: Theme.of(context).cardColor),
+              child: TextComposer(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TextComposer extends StatefulWidget {
+  @override
+  _TextComposerState createState() => _TextComposerState();
+}
+
+class _TextComposerState extends State<TextComposer> {
+  bool _isComposing = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS
+            ? BoxDecoration(
+                border: Border(top: BorderSide(color: Colors.green[200])))
+            : null,
+        child: Row(
+          children: <Widget>[
+            Container(
+              child: IconButton(
+                icon: Icon(Icons.photo_camera),
+                onPressed: () {},
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                onChanged: (text) {
+                  setState(() {
+                    _isComposing = !(text == null || text.isEmpty);
+                  });
+                },
+                decoration:
+                    InputDecoration.collapsed(hintText: "Enviar uma Mensagem"),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4),
+              child: Theme.of(context).platform == TargetPlatform.iOS
+                  ? CupertinoButton(
+                      child: Text("Enviar"),
+                      onPressed: _isComposing ? () {} : null,
+                    )
+                  : IconButton(
+                      onPressed: _isComposing ? () {} : null,
+                      icon: Icon(Icons.send),
+                    ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
